7c446f557bad24f1bf8c685baf0c7c8988f6366e
Adamfomin-skc <96421014+Adamfomin-skc@users.noreply.github.com>
2022-01-12 10:33:49 +0000

    Add files via upload

---
+++ week 1 coding/Class notes 080921.py
@@ -0,0 +1,8 @@
+print('python is a programming language to get the computer to complete a TASK')
+print('thonny is the IDE we are using')
+print('python has a syntax which is a set of rules it must follow')
+print('python has 33 key words or commands which are specially rserved to complete a task')
+print('an ide is an integrated development enviroment eg(thonny)')
+
+
+#this is to explain the purpose of the above code, it helps coders who work as part of a team.
---
+++ week 1 coding/string vs integer 100921.py
@@ -0,0 +1,4 @@
+print(21+10)
+print('21+10')
+
+#this code shows that '' (quotations) determines whether input is a number or a string.
